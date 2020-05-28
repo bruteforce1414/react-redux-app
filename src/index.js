692bbc06e48498105750fefc0b26b0f689eaefab
bruteforce1414 <bruteforce1414@gmail.com>
2020-05-28 14:18:58 +0500

    status: errors occured

--- src/index.js
+++ src/index.js
@@ -1,33 +1,22 @@
-import {createStore, bindActionCreators} from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import reducer from './reducer';
 import * as actions from './actions';
+import App from './components/app';
 
 
 const store=createStore(reducer);
-const {dispatch}=store;
-
-const {inc, dec, rnd}=bindActionCreators(actions, dispatch);
 
 
-
-document.getElementById('inc').addEventListener('click', inc);
-
-document.getElementById('dec').addEventListener('click', dec);
-
-document.getElementById('rnd').addEventListener('click', ()=>{
-    const value=Math.floor(Math.random()*10);      
-
-    rnd(value);
-
-});
+    ReactDOM.render(
+    <Provider store={store}>
+         <App/>
+    </Provider>
+    , document.getElementById('root'));
+        
 
 
-const update=()=>{
-    document.getElementById('counter').textContent=store.getState()
-};
-
-
-
-store.subscribe(update)
 
 
