0afc8e70a0d3169a153176250720e7d9110567ad
bruteforce1414 <bruteforce1414@gmail.com>
2020-05-29 12:00:22 +0500

    feat: it works

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducer';
-import * as actions from './actions';
 import App from './components/app';
 
 
